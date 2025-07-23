9ac199cdd8702e1ebbb9cb7d02b205fd06e8e63c
abiliojunior <abiliojunior.mat@gmail.com>
2025-07-22 21:31:10 -0300

    Incial

---
+++ exercicios/capitulo01/exercicio_1_1.m
@@ -0,0 +1,12 @@
+% Livro: Engenharia de Controle Moderno - Ogata (5a Ed.)
+% Capítulo 1 - Exercício 1.x
+% Resolvedor: Abílio Costa da Silva Júnior
+% Data: YYYY-MM-DD
+
+clear; clc; close all;
+
+% Enunciado: [Copie ou resuma o enunciado aqui]
+
+% Solução:
+
+% [Seu código aqui]
